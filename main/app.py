dea67130d53d5760ace3fa5074833e3d64063b01
2023ac05467 <2023ac05467@wilp.bits-pilani.ac.in>
2025-08-24 01:37:01 +0530

    Update app.py

--- main/app.py
+++ main/app.py
@@ -58,8 +58,10 @@ def get_hf_token():
     return st.secrets.get("HUGGINGFACE", {}).get("TOKEN", None)
 
 def get_groq_key():
-    return "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"
-#    return st.secrets.get("GROQ", {}).get("API_KEY", "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")
+    api_key = st.secrets["groq_api_key"]
+    return api_key
+#   return "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"
+#   return st.secrets.get("GROQ", {}).get("API_KEY", "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")
 
 
 # =====================================
